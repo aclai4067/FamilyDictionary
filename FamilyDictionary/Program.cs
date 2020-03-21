012e1c30b564fa7550ffa806f1bcd903d03807c3
Ashley Claiborne <languageisaliquid@gmail.com>
2020-03-21 11:31:56 -0500

    Add project files.

---
+++ FamilyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FamilyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var familyDictionary = new Dictionary<string, Dictionary<string, string>>();
+            familyDictionary.Add("mother", new Dictionary<string, string>() { { "name", "Janie" }, { "age", "66" } });
+            familyDictionary.Add("father", new Dictionary<string, string>() { { "name", "Tom" }, { "age", "71" } });
+            familyDictionary.Add("sister", new Dictionary<string, string>() { { "name", "Mandy" }, { "age", "37" } });
+
+            foreach (var (relationship, familyMember) in familyDictionary)
+            {
+                Console.WriteLine($"{familyMember["name"]} is my {relationship} and is {familyMember["age"]} years old.");
+            }
+        }
+    }
+}
